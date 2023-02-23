6a74d05c5b2090fedbe21ab1eaa8ca7b69de43c5
danielpennypacker <daniel.pennypacker@clio.com>
2023-02-23 12:27:41 -0800

    adding basic jwt interaction

--- client/app/controllers/home_controller.rb
+++ client/app/controllers/home_controller.rb
@@ -1,12 +1,39 @@
 class HomeController < ApplicationController
   def new
+    require 'net/http'
 
     puts 'client === sending message ===='
 
-    require 'net/http'
 
-    url = URI.parse('http://localhost:3001/home/new')
+    puts 'client === RSA Example ===='
+    payload = {hi: 'there'}
+    rsa_private = OpenSSL::PKey::RSA.generate 2048
+    rsa_public = rsa_private.public_key
+    token = JWT.encode payload, rsa_private, 'PS256'
+    puts token
+    decoded_token = JWT.decode token, rsa_public, true, { algorithm: 'PS256' }
+    puts decoded_token
+
+
+    # Example with HMAC, i.e. a secret.
+    puts 'client === HMAC Example ===='
+    hmac_secret = 'super-secret'
+    token = JWT.encode payload, hmac_secret, 'HS256'
+    puts token
+
+    decoded_token = JWT.decode token, hmac_secret, true, { algorithm: 'HS256' }
+
+    # Array
+    # [
+    #   {"data"=>"test"}, # payload
+    #   {"alg"=>"HS256"} # header
+    # ]
+    puts decoded_token
+
+
+    url = URI.parse("http://localhost:3001/home/new")
     req = Net::HTTP::Get.new(url.to_s)
+    req['blah-token'] = token
     res = Net::HTTP.start(url.host, url.port) {|http|
       http.request(req)
     }
--- provider/app/controllers/home_controller.rb
+++ provider/app/controllers/home_controller.rb
@@ -1,8 +1,13 @@
 class HomeController < ApplicationController
   def new
+    token = request.headers["blah-token"]
 
-    puts 'provider ----- request recieved'
+    puts 'provider ----- token '
+    puts token
+    hmac_secret = 'super-secret'
+    decoded_token = JWT.decode token, hmac_secret, true, { algorithm: 'HS256' }
 
-
+    puts 'provider ---- decoded token -----'
+    puts decoded_token
   end
 end
